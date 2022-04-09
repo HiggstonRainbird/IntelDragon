8baa24323f11c89f124fbb154f42b10db8079dc7
HiggstonRainbird <fberg.david@gmail.com>
2022-04-09 11:18:37 -0400

    Made news article formatting fancier.

--- IntelDragon.py
+++ IntelDragon.py
@@ -69,6 +69,52 @@ sentences = np.array(article_sentences)
 
 st.title("IntelDragon")
 
+with st.expander("Word Similarity", expanded=False):
+	with st.form(key="word_similarity_form"):
+
+		similarityInputPositive = st.text_area("Like these words:", "WannaCry")
+		similarityInputNegative = st.text_area("Unlike these words:", "")
+		similarityColumns = ["Word", "Similarity"]
+
+		similarityOutput = \
+			pd.DataFrame(
+			model.wv.most_similar(
+				positive=convert_to_tokens(similarityInputPositive), 
+				negative=convert_to_tokens(similarityInputNegative))
+			)
+		similarityOutput.columns = similarityColumns
+
+		st.table(similarityOutput)
+		# st.markdown(f"")
+
+		submit_button_similarity = st.form_submit_button(label="Refresh results")
+
+with st.expander("One of These Things is Not Like the Others", expanded=False):
+	with st.form(key="odd_man_out_form"):
+
+		oddManOutInput = st.text_area("", "WannaCry Bitcoin Ryuk Petya REvil")
+		parsedInput = convert_to_tokens(oddManOutInput)
+		# "WannaCry Bitcoin ryuk petya REvil" #EternalBlue
+
+		oddManOutOutput = model.wv.doesnt_match(parsedInput) #EternalBlue
+
+		submit_button_oddManOut = st.form_submit_button(label="Refresh results")
+
+		# Make similarity matrix of the input words.
+		similarityMatrix = []
+		for i in range(0,len(parsedInput)):
+			tmpMatrix = []
+			for j in range(0, len(parsedInput)):
+				tmpMatrix.append(model.wv.similarity(parsedInput[i], parsedInput[j]))
+			similarityMatrix.append(tmpMatrix)
+		
+		st.markdown("## Least Similar:")
+		st.markdown(f"{oddManOutOutput}")
+
+		st.markdown("## Similarity Matrix")
+		similarityMatrix = pd.DataFrame(similarityMatrix, columns=parsedInput, index=parsedInput)
+		st.table(similarityMatrix.style.format("{:.3f}").highlight_min(axis=0))
+
 initial_input_text = "I'm mostly interested in Bitcoin, Ethereum, and potential financial and cryptocurrency scams."
 with st.form(key="relevant_article_form"):
 	input_text = st.text_area("Your interests and needs:", initial_input_text)
@@ -103,76 +149,47 @@ with st.form(key="relevant_article_form"):
 	sorted_articles.sort(key=lambda l: -l[-1])
 
 	sorted_articles_df = pd.DataFrame(sorted_articles, columns=["article_id", "key_sentence", "relevance"])
-	
-	# # Print the lengths of all sentences.
-	# for i in range(0,len(sentences)):
-	# 	print(len(sentences[i]))
-
-	# Display the text corresponding to the top 10 articles.
-	for i in range(0,5):
-		article_id = sorted_articles_df["article_id"][i]
-		st.markdown(f"[{article_titles[article_id]}]({article_url[article_id]})")
-		#st.markdown(f"{article_sentences[article_id]}")
-		# Key sentence
-		st.markdown(f"{sentences[article_id][sorted_articles_df['key_sentence'][i]]}")
-		st.markdown(f"{sorted_articles_df['relevance'][i]}")
-		print("")
-		#st.markdown("")
-	
-	
-	# for a, score in sorted_articles[:10]:
-	# 	print(article_titles[a])
-	# 	print(score)
-	# 	print("")
-	# st.table(sorted_articles_df[:5])
 
 	submit_button_relevantArticles = st.form_submit_button("Find Relevant Articles")
 
-with st.expander("Word Similarity", expanded=False):
-	with st.form(key="word_similarity_form"):
+# Display the text corresponding to the top 5 articles.
+col0, col1, col2, col3, col4 = st.columns(5)
+with col0:
+	i = 0
+	article_id = sorted_articles_df["article_id"][i]
+	st.markdown(f"**[{article_titles[article_id]}]({article_url[article_id]})**")
+	st.markdown(f"*Relevance: {round(sorted_articles_df['relevance'][i],3)}*")
+	st.markdown(f"{sentences[article_id][sorted_articles_df['key_sentence'][i]]}")
+	st.markdown("")
 
-		similarityInputPositive = st.text_area("Like these words:", "WannaCry")
-		similarityInputNegative = st.text_area("Unlike these words:", "")
-		similarityColumns = ["Word", "Similarity"]
+with col1:
+	i = 1
+	article_id = sorted_articles_df["article_id"][i]
+	st.markdown(f"**[{article_titles[article_id]}]({article_url[article_id]})**")
+	st.markdown(f"*Relevance: {round(sorted_articles_df['relevance'][i],3)}*")
+	st.markdown(f"{sentences[article_id][sorted_articles_df['key_sentence'][i]]}")
+	st.markdown("")
 
-		similarityOutput = \
-			pd.DataFrame(
-			model.wv.most_similar(
-				positive=convert_to_tokens(similarityInputPositive), 
-				negative=convert_to_tokens(similarityInputNegative))
-			)
-		similarityOutput.columns = similarityColumns
+with col2:
+	i = 2
+	article_id = sorted_articles_df["article_id"][i]
+	st.markdown(f"**[{article_titles[article_id]}]({article_url[article_id]})**")
+	st.markdown(f"*Relevance: {round(sorted_articles_df['relevance'][i],3)}*")
+	st.markdown(f"{sentences[article_id][sorted_articles_df['key_sentence'][i]]}")
+	st.markdown("")
 
-		st.table(similarityOutput)
+with col3:
+	i = 3
+	article_id = sorted_articles_df["article_id"][i]
+	st.markdown(f"**[{article_titles[article_id]}]({article_url[article_id]})**")
+	st.markdown(f"*Relevance: {round(sorted_articles_df['relevance'][i],3)}*")
+	st.markdown(f"{sentences[article_id][sorted_articles_df['key_sentence'][i]]}")
+	st.markdown("")
 
-		st.markdown(f"{dist}")
-		# st.markdown(f"")
-
-		submit_button_similarity = st.form_submit_button(label="Refresh results")
-
-with st.expander("One of These Things is Not Like the Others", expanded=False):
-	with st.form(key="odd_man_out_form"):
-
-		oddManOutInput = st.text_area("", "WannaCry Bitcoin Ryuk Petya REvil")
-		parsedInput = convert_to_tokens(oddManOutInput)
-		# "WannaCry Bitcoin ryuk petya REvil" #EternalBlue
-
-		oddManOutOutput = model.wv.doesnt_match(parsedInput) #EternalBlue
-
-		submit_button_oddManOut = st.form_submit_button(label="Refresh results")
-
-		# Make similarity matrix of the input words.
-		similarityMatrix = []
-		for i in range(0,len(parsedInput)):
-			tmpMatrix = []
-			for j in range(0, len(parsedInput)):
-				tmpMatrix.append(model.wv.similarity(parsedInput[i], parsedInput[j]))
-			similarityMatrix.append(tmpMatrix)
-		
-		st.markdown("## Least Similar:")
-		st.markdown(f"{oddManOutOutput}")
-
-		st.markdown("## Similarity Matrix")
-		similarityMatrix = pd.DataFrame(similarityMatrix, columns=parsedInput, index=parsedInput)
-		similarityMatrix.round(3)
-		st.table(similarityMatrix)
+with col4:
+	i = 4
+	article_id = sorted_articles_df["article_id"][i]
+	st.markdown(f"**[{article_titles[article_id]}]({article_url[article_id]})**")
+	st.markdown(f"*Relevance: {round(sorted_articles_df['relevance'][i],3)}*")
+	st.markdown(f"{sentences[article_id][sorted_articles_df['key_sentence'][i]]}")
+	st.markdown("")
